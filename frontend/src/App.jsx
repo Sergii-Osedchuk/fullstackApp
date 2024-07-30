86c1eab48e25b8a325e29384d21ac1820aaa9fa2
Sergio <norgestream@gmail.com>
2024-07-30 20:06:59 +0300

    initial project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/Navbar/NavBar";
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar />
+    </div>
+  )
+};
+
+export default App;
---
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+
+    </div>
+  )
+}
+
+export default NavBar
