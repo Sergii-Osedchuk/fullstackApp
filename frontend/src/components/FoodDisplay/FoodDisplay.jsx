cd45d06ff83c4b2ffc64f47ef59874e3a8559229
Sergio <norgestream@gmail.com>
2024-07-31 20:10:25 +0300

    add state counter for dish add styling for card with dish and card list

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
+
+function FoodDisplay({category}) {
+  const { food_list } = useContext(StoreContext);
+  return (
+    <div className='food_display' id='food-display'>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item) => {
+          return (
+            <FoodItem key={ item._id} id={item._id} name={item.name} description={item.description} price={item.price} image={ item.image } />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import './Home.css';
 import { useState } from 'react';
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 
 function Home() {
   const[category, setCategory] = useState('All');
   return (
     <div>
       <Header />
-      <ExploreMenu category={category} setCategory={ setCategory } />
+      <ExploreMenu category={category} setCategory={setCategory} />\
+      <FoodDisplay category={ category } />
     </div>
   )
 }
