98ac7fd460a5583a28e332f791c3724fa522c942
Sergio <norgestream@gmail.com>
2024-08-01 20:42:51 +0300

    add responsive design to homepage

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,10 +1,14 @@
 import './AppDownload.css';
+import { assets } from '../../assets/assets';
 
 function AppDownload() {
   return (
     <div className='app-download' id='app=download'>
       <p>For Better Experiance Download <br /> Tomato App</p>
-
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
     </div>
   )
 }
