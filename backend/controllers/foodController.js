06bda5de60cd9389e0193c1fb122d519ab862b52
Sergio <norgestream@gmail.com>
2024-08-03 17:37:55 +0300

    add controllers for managing routes

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,10 +1,55 @@
+import { log } from "console";
 import foodModal from "../models/foodModel.js";
 import fs from "fs";
 
 //add food item
 
 const addFood = async (req, res) => {
-  
+
+  let image_filename = `${req.file.filename}`;
+
+  const food = new foodModal({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename
+  })
+  try {
+    await food.save();
+    res.json({success: true, message: "Food added"})
+  } catch (error) {
+    console.log(error)
+    res.json({success: false, message: "Error"})
+  }
 }
 
-export {addFood}
+//all food list
+
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModal.find({});
+    res.json({ success: true, data:foods });
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: "Error"})
+    
+  }
+}
+
+//remove food
+
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModal.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => { })
+
+    await foodModal.findByIdAndDelete(req.body.id);
+    res.json({success: true, message: "Food removed"})
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: "Error"})
+  }
+}
+
+export { addFood, listFood, removeFood };
