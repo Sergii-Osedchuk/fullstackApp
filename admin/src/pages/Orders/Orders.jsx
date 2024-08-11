b7a028b6ded54890c0d1c103b712e991ee0d7e95
Sergio <norgestream@gmail.com>
2024-08-11 12:34:05 +0300

    add ability to update order status in admin panel

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,22 +1,31 @@
-import './Orders.css';
+import "./Orders.css";
 import axios from "axios";
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback } from "react";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { assets } from "../../assets/assets.js";
 
-function Orders({url}) {
+function Orders({ url }) {
   const [orders, setOrders] = useState([]);
 
   const fetchAllOrders = useCallback(async () => {
     const response = await axios.get(url + "/api/order/list");
     if (response.data.success) {
       setOrders(response.data.data);
+    } else {
+      toast.error("Error");
     }
-    else {
-      toast.error("Error")
+  }, [url]);
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value
+    })
+    if (response.data.success) {
+      await fetchAllOrders();
     }
-  },[url])
+  }
 
   useEffect(() => {
     fetchAllOrders();
@@ -34,18 +43,40 @@ function Orders({url}) {
                 {order.items.map((item, index) => {
                   if (index === order.items.length - 1) {
                     return item.name + " x " + item.quantity;
-                  }
-                  else {
-                    return item.name + " x " + item.quantity + ", "
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
                   }
                 })}
               </p>
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              <div className="order-item-address">
+                <p>{order.address.street + ","}</p>
+                <p>
+                  {order.address.city +
+                    ", " +
+                    order.address.state +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipcode}
+                </p>
+              </div>
+              <p className="order-item-phone">{order.address.phone}</p>
             </div>
+            <p>Items : {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select onChange={(event) => statusHandler(event, order._id) } value={order.status}>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Orders
+export default Orders;
